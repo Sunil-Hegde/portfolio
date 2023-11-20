33161b4adef87e876df7186a8f6dc94ea5b0363b
Sunil-Hegde <sunil1hegde@gmail.com>
2023-11-20 17:32:55 +0530

    Added a nav screen for smaller screens

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 const toggleSwitch = document.querySelector('.l');
 const helloFont = document.querySelector('.helloText');
+const hamburgerSwitch = document.querySelector('.hamburger input');
+const navFont = document.querySelectorAll('.overlay-content a');
 
 toggleSwitch.addEventListener('change', function() {
     if (this.checked) {
         document.body.style.backgroundColor = '#f5fffa';
         helloFont.style.color = '#21251f';
+        navFont.forEach(link => {
+            link.style.color = '#21251f';
+        });
+        navFont.forEach(link => {
+            link.addEventListener('mouseover', function () {
+                this.style.color = 'green';
+            });
+            link.addEventListener('mouseout', function () {
+                this.style.color = '#21251f';
+            });
+        });
     } else {
         // Toggle is unchecked, revert to original colors
         document.body.style.backgroundColor = '#21251f';
         helloFont.style.color = '#a3fda1';
+        navFont.forEach(link => {
+            link.style.color = '#925ff0';
+        });
+        navFont.forEach(link => {
+            link.addEventListener('mouseover', function () {
+                this.style.color = 'red';
+            });
+            link.addEventListener('mouseout', function () {
+                this.style.color = '#925ff0';
+            });
+        });
     }
 });
+
+hamburgerSwitch.addEventListener('change', function() {
+    if (this.checked) {
+        openNav();
+    } else {
+        closeNav();
+    }
+});
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
